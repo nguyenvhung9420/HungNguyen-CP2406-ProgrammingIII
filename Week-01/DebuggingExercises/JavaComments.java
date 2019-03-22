25c7d5d3b52f94b6d571fc9027274030d8ecf22e
Hung Nguyen <nguyenvhung@laposte.net>
2019-03-22 11:18:36 +0800

    Done with 2 any exercises

---
+++ Week-01/DebuggingExercises/JavaComments.java
@@ -0,0 +1,5 @@
+public class JavaComments{
+    public static void main(String[] args){
+        System.out.println("\"Programme comments are non-executing statements you add to a file for the purpose of documentation.\"");
+    }
+}
